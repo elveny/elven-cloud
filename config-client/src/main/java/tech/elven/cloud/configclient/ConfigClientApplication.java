316391dd160ac319a9bf701299468a4401f3f3ae
elveny <elve1984@163.com>
2017-09-21 01:41:38 +0800

    添加config-server 添加config-client

---
+++ config-client/src/main/java/tech/elven/cloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,30 @@
+/**
+ * elven.tech Inc.
+ * Copyright (c) 2017-2026 All Rights Reserved.
+ */
+package tech.elven.cloud.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Filename ConfigClientApplication.java
+ *
+ * @description
+ *
+ * @Version 1.0
+ *
+ * @author elven
+ * @History
+ *
+ * <li>Author: elven</li>
+ * <li>Date: 17-9-21 上午1:13</li>
+ * <li>Version: 1.0</li>
+ * <li>Content: create</li>
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
