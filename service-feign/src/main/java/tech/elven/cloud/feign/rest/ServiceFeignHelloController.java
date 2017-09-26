4d2287bf0b53f34547c23b2b0affcd2935ceff9d
qiusheng.wu <qiusheng.wu@msxf.com>
2017-09-26 20:19:34 +0800

    gradle 打包子工程

--- service-feign/src/main/java/tech/elven/cloud/feign/rest/ServiceFeignHelloController.java
+++ service-feign/src/main/java/tech/elven/cloud/feign/rest/ServiceFeignHelloController.java
@@ -7,6 +7,7 @@ package tech.elven.cloud.feign.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,7 @@ public class ServiceFeignHelloController {
     @Value("${server.port}")
     private String port;
 
+    @Qualifier("elven-cloud-service-provider")
     @Autowired
     private HelloService helloService;
 
